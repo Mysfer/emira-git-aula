6a4d9d4938d71b419488ccb9cd333324ab90e0cf
MuCaMartins <murilo.camilo.martins@hotmail.com>
2018-02-23 14:00:32 -0300

    Formulario completo

---
+++ index.php
@@ -0,0 +1,41 @@
+!DOCTYPE html>
+<html>
+    <header class="header" id="top">
+            <div class="text-vertical-center">
+              
+            </div>
+        </header>
+<head>
+	<title>Emira aula</title>
+	<meta charset="iso-8859-1">
+
+</head>
+<body>
+	<h2>Reclamações Emira aula</h2>
+	
+    <form method="post" id="formulario_contato" onsubmit="validaForm(); return false;" class="form">
+		<p class="name">
+            <label for="name">Nome</label>
+            <input type="text" name="nome" id="nome" placeholder="Seu Nome" />
+		</p>
+
+        	<p class="email">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email" placeholder="Seu Email" />
+		</p>
+		
+		<p class="Mensagem">
+            <label for="name">Mensagem</label>
+            <input type="text" name="mensagem" id="mensagem" placeholder="Sua Mensagem" />
+		</p>
+        <p class="submit">
+            <input type="submit" value="Enviar" />
+		</p>
+	
+		<p class="text">
+            <label for="mensagem">Últimas Mensagens</label>
+            
+		</p>
+		
+		
+	</form>
